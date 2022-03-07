052eff85bb1761f960d566d3429eaa237ab1f52e
BozhidarA <bplatanasov@gmail.com>
2022-03-07 23:46:34 +0200

    added css to 404 page

--- src/pages/FourOFour.tsx
+++ src/pages/FourOFour.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import IPageProps from '../interfaces/page';
+import styles from "../styles/Components.module.css"
 
 const FourOFour: React.FunctionComponent<IPageProps> = props => {
     return (
         <div>
-            <h1>404 Page not found</h1>
+            <h1 className={styles.WhiteCenteredText}>404 Page not found</h1>
         </div>
     );
 }
